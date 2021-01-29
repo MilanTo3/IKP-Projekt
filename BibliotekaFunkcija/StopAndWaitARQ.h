347186ccf373b3a9685160ab4fcef1163ce8cbcd
maka031 <mlazovic1998@gmail.com>
2021-01-29 23:24:20 +0100

    Sitne izmene, brisanje foldera...

--- BibliotekaFunkcija/StopAndWaitARQ.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <ws2tcpip.h>
-#include "stdio.h"
-#include "stdlib.h"
-
-int snwarq_sendto(SOCKET uticnica, char* data, int duzinapodataka, int flag, const sockaddr* destination, int destinationlen);
-int snwarq_recvfrom(SOCKET uticnica, char* data, int duzinapodataka, int flag, const sockaddr* clientaddress, int *clientlen);
